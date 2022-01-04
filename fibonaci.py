7d12cfdd69f8a5e7c686de6e94a56854db13d497
ydstiroa <47847548+ydstiroa@users.noreply.github.com>
2022-01-04 20:37:12 +0700

    Update fibonaci.py

--- fibonaci.py
+++ fibonaci.py
@@ -9,20 +9,20 @@ def fibonaci(num):
     n2=1 
     count=0
     list=[]
-    while count < num:
+    while count < num:      #melakukan looping selama nilai count kurang dari nilai num
         list.append(n1)     #masukkan bilangan kedalam  list
         temp = n1 + n2      #inisiasi bilangan selanjutnya
         n1 = n2             #ubah n1 menjadi n2
         n2 = temp           #ubah n2 menjadi temp
         count += 1          #tambahkan batasan
         
-    for i in range(len(list)):
+    for i in range(len(list)): #melakukan looping untuk mengembalikan nilai dalam list
         return list
 
-num = int(input("input number: "))
+num = int(input("input number: ")) #input nilai batas untuk menghitung fibonaci
 
-a = fibonaci(num)
-print(a)
+a = fibonaci(num) #memanggil fungsi fibonaci
+print(a) #cetak hasil 
 
 # while count < num:
 #     list.append(n1)
